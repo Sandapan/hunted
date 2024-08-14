8215e3528a14cdf091d456cdc8cc2fefcc122e86
Sandapan <powdecolle@gmail.com>
2024-08-15 00:35:51 +0200

    Début

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
         io.emit('roomsList', Object.keys(rooms).map(id => ({ id, host: rooms[id].hostUsername })));
     });
     
-    //test fonctionne !
+
 
     socket.on('getRooms', () => {
         const availableRooms = Object.keys(rooms).map(id => ({
