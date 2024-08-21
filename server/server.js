365d086eb22bf50e6855a242a1d53e1b2d51ae82
Sandapan <powdecolle@gmail.com>
2024-08-21 12:19:40 +0200

    v2

--- server/server.js
+++ server/server.js
@@ -6,9 +6,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const path = require('path'); // Utilisation du module path pour manipuler les chemins de fichiers
+
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+const items = require('./objets'); // utilise le fichier objets.js 
+
 
 const rooms = {};
 
@@ -19,37 +25,6 @@ const roomNames = [
     'la salle des tortures',
     'la salle du trône',
     "l'église"
-];
-
-
-// LISTE D OBJETS MISE EN PLACE INVENTAIRE
-
-const items = [
-    {
-        name: 'Potion de soin',
-        description: 'Consommable redonnant 1 HP',
-        image: '/images/healing_potion.png',  // Chemin relatif à public
-        effect: (player) => { player.hp += 1; }
-    },
-    {
-        name: 'Petite bourse d\'or',
-        description: 'Contient 50 pièces d\'or',
-        image: '/images/small_gold_pouch.png',  // Chemin relatif à public
-        effect: (player) => { player.gold += 50; }
-    },
-    {
-        name: 'Fortune',
-        description: 'Contient 100 pièces d\'or',
-        image: '/images/fortune.png',  // Chemin relatif à public
-        effect: (player) => { player.gold += 100; }
-    },
-    {
-        name: 'Somptueux trésor',
-        description: 'Contient 250 pièces d\'or',
-        image: '/images/luxurious_treasure.png',  // Chemin relatif à public
-        effect: (player) => { player.gold += 250; }
-    },
-    // Vous pouvez ajouter plus d'objets ici à l'avenir
 ];
 
 
